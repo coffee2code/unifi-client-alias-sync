24c57f151199f9501f730ee02e3807bb2a5c9297
Scott Reilly <scott@coffee2code.com>
2018-03-06 16:23:33 -0700

    Unit tests: Add tests for `prioritize_sites()` and `UNIFI_ALIAS_SYNC_PRIORITIZED_SITES`

--- tests/test-sites.php
+++ tests/test-sites.php
@@ -54,5 +54,58 @@ final class UniFiClientAliasSitesTest extends UniFiClientAliasTestBase {
 		$this->assertTrue( $this->arrays_are_ordered( $_sites, $sites ) );
 	}
 
+	public function test_prioritize_sites_puts_default_first() {
+		$test = self::get_method( 'prioritize_sites' );
+
+		$sites = $test->invokeArgs( UniFi_Client_Alias_Sync\TestSyncer::get_instance(), [ self::$sites ] );
+		reset( $sites );
+
+		$this->assertEquals( 'default', key( $sites ) );
+	}
+
+	public function test_prioritize_sites_single() {
+		$_sites = self::$sites;
+
+		$this->set_config( 'UNIFI_ALIAS_SYNC_PRIORITIZED_SITES', [ '9lirxq5p' ] );
+
+		$test = self::get_method( 'prioritize_sites' );
+
+		$sites = $test->invokeArgs( UniFi_Client_Alias_Sync\TestSyncer::get_instance(), [ $_sites ] );
+		reset( $sites );
+
+		$this->assertEquals( '9lirxq5p', key( $sites ) );
+
+		// Get full ordered array.
+		$site = $_sites[ '9lirxq5p' ];
+		unset( $_sites[ '9lirxq5p' ] );
+		$_sites = $this->sort_sites( $_sites );
+		$_sites = array_merge( [ '9lirxq5p' => $site ], $_sites );
+
+		$this->assertTrue( $this->arrays_are_ordered( $_sites, $sites ) );
+	}
+
+	public function test_prioritize_sites_multiple() {
+		$_sites = self::$sites;
+
+		$this->set_config( 'UNIFI_ALIAS_SYNC_PRIORITIZED_SITES', [ '1qwe314gn', 'cd90qe2s' ] );
+
+		$test = self::get_method( 'prioritize_sites' );
+
+		$sites = $test->invokeArgs( UniFi_Client_Alias_Sync\TestSyncer::get_instance(), [ $_sites ] );
+		reset( $sites );
+
+		$this->assertEquals( '1qwe314gn', key( $sites ) );
+		$this->assertEquals( 'cd90qe2s', array_keys( $sites )[1] );
+		$this->assertEquals( 'default', array_keys( $sites )[2] );
+
+		// Get full ordered array.
+		$p_sites = [ '1qwe314gn' => $_sites[ '1qwe314gn'], 'cd90qe2s' => $_sites[ 'cd90qe2s' ] ];
+		unset( $_sites[ '1qwe314gn' ] );
+		unset( $_sites[ 'cd90qe2s' ] );
+		$_sites = $this->sort_sites( $_sites );
+		$_sites = array_merge( $p_sites, $_sites );
+
+		$this->assertTrue( $this->arrays_are_ordered( $_sites, $sites ) );
+	}
 
 }
