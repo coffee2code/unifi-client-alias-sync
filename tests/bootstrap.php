11f895618ef4553bc32678a9ee5a72939522c5ad
Scott Reilly <scott@coffee2code.com>
2018-03-07 01:07:00 -0700

    Unit tests: Set constant `UNIFI_ALIAS_SYNC_TESTING` to true in bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,6 +8,8 @@
 ini_set( 'display_errors', 'on' );
 error_reporting( E_ALL );
 
+define( 'UNIFI_ALIAS_SYNC_TESTING', true );
+
 require dirname( __DIR__ ) . DIRECTORY_SEPARATOR . 'unifi-client-alias-sync.php';
 require __DIR__ . DIRECTORY_SEPARATOR . 'class-test-base.php';
 require __DIR__ . DIRECTORY_SEPARATOR . 'class-test-syncer.php';
